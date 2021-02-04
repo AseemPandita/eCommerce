599f80f53b4f4082f5a1a3c6db4d79451af4c7b1
Aseem Pandita <aseempandita@live.com>
2021-02-04 14:14:49 +0530

    Custom errors

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
-
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 const router = express.Router();
 
 router.post(
@@ -16,11 +17,12 @@ router.post(
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      throw new RequestValidationError(errors.array());
     }
 
-    const { email, password } = req.body;
+    // const { email, password } = req.body;
 
+    throw new DatabaseConnectionError();
     console.log('Creating user...');
 
     res.send({});
