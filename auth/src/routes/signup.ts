cfbfad2cfce02f27343b2ce33304618c4882871e
Aseem Pandita <aseempandita@live.com>
2021-02-20 14:01:29 +0530

    user model, password, signup route, 404 route

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { BadRequestError } from '../errors/bad-request-error';
+
 const router = express.Router();
 
 router.post(
@@ -13,19 +15,26 @@ router.post(
       .isLength({ min: 6, max: 20 })
       .withMessage('Password must be between 6 and 20 characters'),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
 
-    // const { email, password } = req.body;
+    const { email, password } = req.body;
 
-    throw new DatabaseConnectionError();
-    console.log('Creating user...');
+    const existingUser = await User.findOne({ email });
 
-    res.send({});
+    if (existingUser) {
+      throw new BadRequestError('Email in use');
+    }
+
+    const user = User.build({ email, password });
+
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
