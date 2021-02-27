3f9da2fd5f81c8593ad5c03a5f40aec0a93b7b85
Aseem Pandita <aseempandita@live.com>
2021-02-27 15:20:00 +0530

    Created the orders service

---
+++ orders/src/app.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@pandita/common';
+
+import { createItemRouter } from './routes/new';
+import { showItemRouter } from './routes/show';
+import { indexItemRouter } from './routes/index';
+import { updateItemRouter } from './routes/update';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+
+app.use(currentUser);
+
+app.use(createItemRouter);
+app.use(showItemRouter);
+app.use(indexItemRouter);
+app.use(updateItemRouter);
+
+app.all('*', async (req, res, next) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
---
+++ orders/src/index.ts
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('Cannot load JWT_KEY');
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error('Cannot load MONGO_URI');
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('Cannot load NATS_CLIENT_ID');
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('Cannot load NATS_URL');
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('Cannot load NATS_CLUSTER_ID');
+  }
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed.');
+      process.exit();
+    });
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('Connected to Mongo DB [/items]');
+  } catch (err) {
+    console.error(err);
+  }
+  app.listen(3000, () => {
+    console.log('Items microservice listening on port 3000 using skaffold...');
+  });
+};
+
+start();
