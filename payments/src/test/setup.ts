731046218ed88a7ac89d2b0866d0e3290fa750f3
Aseem Pandita <aseempandita@live.com>
2021-03-07 01:15:19 +0530

    payments -init

---
+++ payments/src/test/setup.ts
@@ -0,0 +1,65 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import { app } from '../app';
+import request from 'supertest';
+import jwt from 'jsonwebtoken';
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): string[];
+    }
+  }
+}
+
+jest.mock('../nats-wrapper');
+
+let mongo: any;
+
+beforeAll(async () => {
+  process.env.JWT_KEY = 'asdf';
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+global.signin = () => {
+  // Build JWT Payload { id, email }
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: 'testemail@emailtest.com',
+  };
+
+  // Create JWT
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  // Build session Object
+  const session = { jwt: token };
+
+  // Turn that into JSON
+  const sessionJSON = JSON.stringify(session);
+
+  // Encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
+  // Return string - cookie data
+  return [`express:sess=${base64}`];
+};
